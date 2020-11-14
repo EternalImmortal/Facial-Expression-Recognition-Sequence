fb697d3291abd7fcca0feb66c4f29512855b97c2
魏仁杰 <weijosephrj@gmail.com>
2020-11-14 22:55:43 +0800

    change to py3

--- mainpro_CK+.py
+++ mainpro_CK+.py
@@ -141,7 +141,8 @@ def test(epoch):
 
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
-        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+        with torch.no_grad():
+            inputs, targets = Variable(inputs), Variable(targets)
         outputs = net(inputs)
         outputs_avg = outputs.view(bs, ncrops, -1).mean(1)  # avg over crops
 
