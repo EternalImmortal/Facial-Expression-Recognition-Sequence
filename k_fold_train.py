48fa3d4a070f6a983051cb28339bf20deb064cdf
魏仁杰 <weijosephrj@gmail.com>
2020-11-14 23:24:42 +0800

    change to py3

--- k_fold_train.py
+++ k_fold_train.py
@@ -8,6 +8,6 @@ for i in range(10):
     print("current fold " + str(i + 1))
     cmd = 'python mainpro_CK+.py --model VGG19 --bs 32 --lr 0.01 --fold %d' % (i + 1)
     if not opt.show_details:
-        cmd += " --show_details False"
+        cmd += " --show_details 0"
     os.system(cmd)
 print("Train VGG19 ok!")
