719241fb064e0e640b8cab65dabb35bfa0a881b2
魏仁杰 <weijosephrj@gmail.com>
2020-11-14 23:06:30 +0800

    change to py3

--- k_fold_train.py
+++ k_fold_train.py
@@ -1,7 +1,13 @@
 import os
+import argparse
+parser = argparse.ArgumentParser(description='PyTorch CK+ CNN Training')
+parser.add_argument('--show_details', default=True, type=bool, help="whether show the progress of each epoch")
+opt = parser.parse_args()
 
 for i in range(10):
     print("current fold " + str(i + 1))
     cmd = 'python mainpro_CK+.py --model VGG19 --bs 32 --lr 0.01 --fold %d' % (i + 1)
+    if opt.show_details:
+        cmd += " --show_details False"
     os.system(cmd)
 print("Train VGG19 ok!")
