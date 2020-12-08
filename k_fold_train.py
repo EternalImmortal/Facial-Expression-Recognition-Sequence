0be7840735e83836c0386cc56251b61b870a588d
魏仁杰 <weijosephrj@gmail.com>
2020-12-08 21:13:51 +0800

    try

--- k_fold_train.py
+++ k_fold_train.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 parser = argparse.ArgumentParser(description='PyTorch CK+ CNN Training')
-parser.add_argument('--show_details', default=1, help="whether show the progress of each epoch")
+parser.add_argument('--hide_details', help="whether show the progress of each epoch")
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--model', default='VGG19')
 opt = parser.parse_args()
