b6bc911ee43f1ee3afb18f9e3c6fde370f23265e
魏仁杰 <weijosephrj@gmail.com>
2020-12-08 20:49:32 +0800

    try

--- k_fold_train.py
+++ k_fold_train.py
@@ -3,11 +3,14 @@ import argparse
 parser = argparse.ArgumentParser(description='PyTorch CK+ CNN Training')
 parser.add_argument('--show_details', default=1, help="whether show the progress of each epoch")
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--model', default='VGG19')
 opt = parser.parse_args()
 
 for i in range(10):
     print("current fold " + str(i + 1))
-    cmd = 'python mainpro_CK+.py --model VGG19 --bs 32 --lr 0.01 --fold %d' % (i + 1)
+    cmd = 'python mainpro_CK+.py --bs 128 --lr 0.01 --fold %d' % (i + 1)
+    if opt.model == 'Resnet18':
+        cmd += "--model Resnet18"
     if opt.show_details:
         cmd += " --show_details 0"
     if opt.resume:
